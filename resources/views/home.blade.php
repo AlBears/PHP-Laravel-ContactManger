362e620a01043639ddd81abde20c3b1ec5eabe9f
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-03 12:28:41 +0300

    welcome page, home page, error pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,15 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+          <div class="jumbotron text-center">
+            <h1>Hello, {{ Auth::user()->name }}!</h1>
+            <p class="lead">
+              Welcome Back to Contact Manager App
+            </p>
+            <p>
+              <a href="{{ route('contacts.index') }}" class="btn btn-primary btn-lg">Manage Your Contacts</a>
+            </p>
+          </div>
         </div>
     </div>
 </div>
